67d599e3e1380a997e1ef6ad50397f539ddf6eda
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-04-23 11:31:47 -0400

    Fix mistake - use apple GFF

--- align/align.sh
+++ align/align.sh
@@ -12,7 +12,7 @@ minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
 # TODO: R1 Q1? haploid ref - haploid query?
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_004115385.1/GCA_004115385.1_ASM411538v1_genomic.fna ref_cds.fa > ASM411538v1.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a ASM411538v1.sam \
@@ -27,7 +27,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: PFR_Mdom_1.0
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_022606005.1/GCA_022606005.1_PFR_Mdom_1.0_genomic.fna ref_cds.fa > PFR_Mdom_1.0.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a PFR_Mdom_1.0.sam \
@@ -42,7 +42,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: Malus_domestica_Gala.pri
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_028456005.1/GCA_028456005.1_Malus_domestica_Gala.pri_genomic.fna ref_cds.fa > Malus_domestica_Gala.pri.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a Malus_domestica_Gala.pri.sam \
@@ -57,7 +57,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: Malus_domestica_Gala.altA
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_028456015.1/GCA_028456015.1_Malus_domestica_Gala.altA_genomic.fna ref_cds.fa > Malus_domestica_Gala.altA.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a Malus_domestica_Gala.altA.sam \
@@ -72,7 +72,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: Malus_domestica_Gala.altB
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/data/GCA_028456065.1/GCA_028456065.1_Malus_domestica_Gala.altB_genomic.fna ref_cds.fa > Malus_domestica_Gala.altB.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a Malus_domestica_Gala.altB.sam \
@@ -87,7 +87,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: drMalDome10.2
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/data/GCA_916050505.2/GCA_916050505.2_drMalDome10.2_genomic.fna ref_cds.fa > drMalDome10.2.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a drMalDome10.2.sam \
@@ -102,7 +102,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: drMalDome11.2
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/data/GCA_916612005.2/GCA_916612005.2_drMalDome11.2_genomic.fna ref_cds.fa > drMalDome11.2.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a drMalDome11.2.sam \
@@ -117,7 +117,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: drMalDome58.3
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_916615275.3/GCA_916615275.3_drMalDome58.3_genomic.fna ref_cds.fa > drMalDome58.3.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a drMalDome58.3.sam \
@@ -132,7 +132,7 @@ anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
 # Assembly: drMalDome5.3
 minimap2 -x splice -t 8 -k 12 -a -p 0.4 -N 20 \
     ../data/GCA_916615385.2/GCA_916615385.2_drMalDome5.3_genomic.fna ref_cds.fa > drMalDome5.3.sam
-anchorwave proali -i Zea_mays.AGPv4.34.gff3 \
+anchorwave proali -i ../data/GCF_002114115.1/genomic.gff \
     -as ref_cds.fa \
     -r ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a drMalDome5.3.sam \
