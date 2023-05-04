ffb59af66ec0ffbec5d3522c485841a91ed50a04
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-05-03 22:44:06 -0400

    Add faSize line

--- wiggle/wiggle.sh
+++ wiggle/wiggle.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+faSize -detailed ../data/GCF_002114115.1/GCF_002114115.1_ASM211411v1_genomic.fna > mesculenta_fasize.txt
+
 while read contig_name end_pos
 do
     _JAVA_OPTIONS=-Xmx256g kotlinc -script wiggle_fromMAFmultiThread.main.kts -- \
