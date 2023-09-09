e610f673b4a949d8824510bc7553376d6d5dee5e
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-09-09 13:28:25 -0400

    Reorganize repo root to be /phg
    Instead of the PHG directory being a subdir of the repo root, just bring all files into the top-level to make organization easier. This removes the need to copy files around (Essentially my alternative to the MakeDefaultDirectoryPlugin)

--- setup.sh
+++ setup.sh
@@ -3,13 +3,3 @@
 ./scripts/download-data.sh
 
 ./scripts/rename-chromosomes.sh
-
-mkdir -p phg
-docker run --name create_phg_dir --rm \
-    -v $PWD/phg/:/phg/ \
-    --user $(id -u | tr "\n" ":" && id -g) \
-    -t maizegenetics/phg:1.4 \
-    /tassel-5-standalone/run_pipeline.pl -debug \
-    -MakeDefaultDirectoryPlugin \
-        -workingDir /phg/ \
-    -endPlugin
