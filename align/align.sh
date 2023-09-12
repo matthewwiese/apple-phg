10070690e251a064088fa5ec6a24efaea6797c7a
Matt Wiese <matthewwiese@users.noreply.github.com>
2023-09-12 11:11:38 -0400

    Use NUM_THREADS for minimap, fix paths for drMalSylv7.2

--- align/align.sh
+++ align/align.sh
@@ -10,12 +10,12 @@ NUM_THREADS=$((TOTAL_THREADS - 2))
 anchorwave gff2seq -r  ../data/reference/GCF_002114115.1_ASM211411v1_genomic.fna \
     -i ../data/reference/genomic.gff \
     -o ref_cds.fa
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/reference/GCF_002114115.1_ASM211411v1_genomic.fna ref_cds.fa > ref.sam
 
 # Assembly: ASM411538v1 (domestica)
 # TODO: R1 Q1? haploid ref - haploid query?
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_004115385.1_ASM411538v1_genomic.fna ref_cds.fa > ASM411538v1.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -30,7 +30,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: PFR_Mdom_1.0 (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_022606005.1_PFR_Mdom_1.0_genomic.fna ref_cds.fa > PFR_Mdom_1.0.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -45,7 +45,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_domestica_Gala.pri (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456005.1_Malus_domestica_Gala.pri_genomic.fna ref_cds.fa > Malus_domestica_Gala.pri.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -60,7 +60,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_domestica_Gala.altA (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456015.1_Malus_domestica_Gala.altA_genomic.fna ref_cds.fa > Malus_domestica_Gala.altA.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -75,7 +75,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_domestica_Gala.altB (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456065.1_Malus_domestica_Gala.altB_genomic.fna ref_cds.fa > Malus_domestica_Gala.altB.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -90,7 +90,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalDome10.2 (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_916050505.2_drMalDome10.2_genomic.fna ref_cds.fa > drMalDome10.2.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -105,7 +105,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalDome11.2 (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_916612005.2_drMalDome11.2_genomic.fna ref_cds.fa > drMalDome11.2.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -120,7 +120,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalDome58.3 (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_916615275.3_drMalDome58.3_genomic.fna ref_cds.fa > drMalDome58.3.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -135,7 +135,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalDome5.3 (domestica)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_916615385.2_drMalDome5.3_genomic.fna ref_cds.fa > drMalDome5.3.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -150,7 +150,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: ASM2079583v1 (sieversii)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_020795835.1_ASM2079583v1_genomic.fna ref_cds.fa > ASM2079583v1.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -165,7 +165,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sieversii.pri (sieversii)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456125.1_Malus_sieversii.pri_genomic.fna ref_cds.fa > Malus_sieversii.pri.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -180,7 +180,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sieversii.altA (sieversii)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456135.1_Malus_sieversii.altA_genomic.fna ref_cds.fa > Malus_sieversii.altA.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -195,7 +195,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sieversii.altB (sieversii)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456155.1_Malus_sieversii.altB_genomic.fna ref_cds.fa > Malus_sieversii.altB.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -210,14 +210,14 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalSylv7.2 (sylvestris)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
-    ../data/GCF_916048215.2_drMalSylv7.2_genomic.fna ref_cds.fa > drMalSylv7.2.sam
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
+    ../data/assemblies/GCF_916048215.2_drMalSylv7.2_genomic.fna ref_cds.fa > drMalSylv7.2.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
     -r ../data/reference/GCF_002114115.1_ASM211411v1_genomic.fna \
     -a drMalSylv7.2.sam \
     -ar ref.sam \
-    -s ../data/GCF_916048215.2_drMalSylv7.2_genomic.fna \
+    -s ../data/assemblies/GCF_916048215.2_drMalSylv7.2_genomic.fna \
     -n anchors_drMalSylv7.2 \
     -R 1 -Q 1 \
     -o drMalSylv7.2.maf \
@@ -225,7 +225,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: drMalSylv7.3 (sylvestris)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_916048215.3_drMalSylv7.3_genomic.fna ref_cds.fa > drMalSylv7.3.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -240,7 +240,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sylvestris.pri (sylvestris)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456055.1_Malus_sylvestris.pri_genomic.fna ref_cds.fa > Malus_sylvestris.pri.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -255,7 +255,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sylvestris.altA (sylvestris)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456085.1_Malus_sylvestris.altA_genomic.fna ref_cds.fa > Malus_sylvestris.altA.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
@@ -270,7 +270,7 @@ anchorwave proali -i ../data/reference/genomic.gff \
     -t $NUM_THREADS
 
 # Assembly: Malus_sylvestris.altB (sylvestris)
-minimap2 -x splice -t 40 -k 12 -a -p 0.4 -N 20 \
+minimap2 -x splice -t $NUM_THREADS -k 12 -a -p 0.4 -N 20 \
     ../data/assemblies/GCA_028456105.1_Malus_sylvestris.altB_genomic.fna ref_cds.fa > Malus_sylvestris.altB.sam
 anchorwave proali -i ../data/reference/genomic.gff \
     -as ref_cds.fa \
